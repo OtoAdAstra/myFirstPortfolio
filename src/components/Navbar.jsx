b49d83815aa09d118396ba3d5d8bcb01fde50b73
Oto <110507303+OtoAdAstra@users.noreply.github.com>
2024-11-03 09:53:36 +0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ function Navbar() {
     return(
        <div className="nav-div">
             <div className="nav-left">
-                <a href="https://github.com/OtoBourbon" target="_blank"><VscGithubInverted /></a>
+                <a href="https://github.com/OtoAdAstra" target="_blank"><VscGithubInverted /></a>
                 <a href="https://www.linkedin.com/in/otosharvashidze/" target="_blank"><FaLinkedin /></a>
-                <a href="https://www.instagram.com/otoexastris/" target="_blank"><FaInstagram /></a>
+                <a href="https://www.instagram.com/otoadastra/" target="_blank"><FaInstagram /></a>
                 <a href="https://x.com/OSharvashidze" target="_blank"><FaXTwitter /></a>
             </div>
             <Darktheme/>
