b29c95bbbda5ff2e5125af9766ad3d42fe1e0c22
OtoBourbon <sharvashidze0712@gmail.com>
2024-12-05 18:49:18 +0400

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,29 +1,30 @@
 import "./Projects.css";
 import bril from "../assets/bril.png";
-import cuk from "../assets/cuk.jpg";
-import musk from "../assets/musk.webp";
+import mariotto from "../assets/mariotto.png";
+import musk from "../assets/musk.png";
 
 function Projects() {
   return (
     <div className="projects" id="projects">
       <h2>Projects:</h2>
+
       <div className="projectcard">
+        <a href="https://mariotto.ge/" target="_blank">
+          <div className="card">
+            <img src={mariotto} />
+            <div className="h3h5">
+              <h3>Mariotto travel</h3>
+              <h5>travel company</h5>
+            </div>
+          </div>
+        </a>
+
         <a href="https://bril.consulting/" target="_blank">
           <div className="card">
             <img src={bril} />
             <div className="h3h5">
               <h3>Bril</h3>
               <h5>m&a company</h5>
-            </div>
-          </div>
-        </a>
-
-        <a href="https://www.facebook.com" target="_blank">
-          <div className="card">
-            <img src={cuk} />
-            <div className="h3h5">
-              <h3>Facebook</h3>
-              <h5>Social network</h5>
             </div>
           </div>
         </a>
